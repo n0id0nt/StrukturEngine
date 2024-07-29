05de2d7f77c9e7c6e4ef0980650b7d3ff57c1e44
n0id0nt <h4bert44@gmail.com>
2024-07-29 21:02:16 +1000

    Added script update function

--- StrukturEngine/scr/Util/skLuaState.cpp
+++ StrukturEngine/scr/Util/skLuaState.cpp
@@ -2,19 +2,37 @@
 #include "raylib.h"
 #include <format>
 
+Struktur::Util::skLuaState::skLuaState()
+{
+	m_lua = sol::state();
+	m_lua.open_libraries(sol::lib::base, sol::lib::package, sol::lib::math);
+	// Change the working directory
+	//DEBUG_PRINT(Engine::GetInstance()->GetProjectDirectory());
+	//m_lua.script(std::format("package.path = '{}Scripts/?.lua'", Engine::GetInstance()->GetProjectDirectory()));
+	m_lua.new_usertype<skLuaState>("Script"
+		//,"property", &ScriptComponent::AddScriptProperty
+		,"update", &skLuaState::m_updateFunction
+		//,"onCollisionEnter", &ScriptComponent::m_onCollisionEnterFunction
+		//,"onCollisionExit", &ScriptComponent::m_onCollisionExitFunction
+		//,"onCollisionPreSolve", &ScriptComponent::m_onCollisionPreSolveFunction
+		//,"onMessage", &ScriptComponent::m_onMessage
+	);
+	m_lua.set("Script", this);
+}
+
 void Struktur::Util::skLuaState::Update(float dt)
 {
 	if (m_update.valid())
 	{
 		sol::protected_function updateScript = m_update;
 
-		sol::protected_function_result result = updateScript();
+		sol::protected_function_result result = updateScript(dt);
 
 		// Check if the execution was successful
 		if (!result.valid()) {
 			// An error occurred; retrieve and handle the error message
 			sol::error err = result;
-			TraceLog(LOG_ERROR, std::format("Lua script execution error: {}", err.what()).c_str());
+			TraceLog(LOG_ERROR, std::format("Lua script execution error: {}", err.what()).c_str() );
 			assert(false);
 		}
 	}
@@ -26,7 +44,7 @@ void Struktur::Util::skLuaState::ScriptFile(const std::string& filename)
 
 	if (m_updateFunction)
 	{
-		m_lua.script("function update() Script.update() end");
+		m_lua.script("function update(dt) Script.update(dt) end");
 		m_update = m_lua["update"];
 	}
 	else
--- StrukturEngine/scr/Util/skLuaState.h
+++ StrukturEngine/scr/Util/skLuaState.h
@@ -11,7 +11,7 @@ namespace Struktur
 		class skLuaState
 		{
 		public:
-			skLuaState() = default;
+			skLuaState();
 			~skLuaState() = default;
 
 			void Update(float dt);
